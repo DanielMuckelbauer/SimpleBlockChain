89143b0459133c11a5a0168d01664b9cf390f15a
Daniel Muckelbauer <dadamuck@hotmail.com>
2022-09-09 13:44:17 +0300

    Create block class

---
+++ SimpleBlockChain/BlockChain/Block.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SimpleBlockChain.BlockChain;
+
+public class Block
+{
+    public string Hash { get; set; }
+    public string PreviousHash { get; set; }
+    private long _nonce;
+    private DateTime _timeStamp;
+    private List<Transaction> _transactions;
+
+    public Block(DateTime timeStamp, List<Transaction> transactions, string hash)
+    {
+        _timeStamp = timeStamp;
+        _transactions = transactions;
+        Hash = hash;
+        _nonce = 0;
+    }
+
+    public void Mine()
+    {
+        while(Hash[..2] != "00")
+        {
+            ++_nonce;
+            Hash = CreateHash();
+        }
+        Console.WriteLine("Block calculated.");
+    }
+
+    private string CreateHash()
+    {
+        using var sha256 = SHA256.Create();
+        var hashInput = PreviousHash + _timeStamp + _transactions + _nonce;
+        var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(hashInput));
+        return Encoding.Default.GetString(hash);
+    }
+}
---
+++ SimpleBlockChain/BlockChain/Transaction.cs
@@ -0,0 +1,5 @@
+﻿namespace SimpleBlockChain.BlockChain;
+
+public class Transaction
+{
+}
