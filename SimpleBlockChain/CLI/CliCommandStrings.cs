b699ee9a19495ac19a4b10f6b5500b2f5ffd6bf3
Daniel Muckelbauer <dadamuck@hotmail.com>
2022-09-10 23:46:13 +0300

    Extract magic strings

---
+++ SimpleBlockChain/CLI/CliCommandStrings.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleBlockChain.CLI;
+
+public static class CliCommandStrings
+{
+    public const string AddTransaction = "Add Transaction";
+    public const string MineBlock = "Mine Block";
+    public const string PrintChain = "Print Chain";
+    public const string Quit = "Quit";
+}
--- SimpleBlockChain/CLI/Prompter.cs
+++ SimpleBlockChain/CLI/Prompter.cs
@@ -8,18 +8,17 @@ public class Prompter : IPrompter
     {
         var input = AnsiConsole.Prompt(new SelectionPrompt<string>()
             .Title("What do you want to do?")
-            .AddChoices( "Add Transaction", "Mine Block", "Print Chain", "Quit")
-        );
+            .AddChoices( CliCommandStrings.AddTransaction, CliCommandStrings.MineBlock, CliCommandStrings.PrintChain, CliCommandStrings.Quit));
         return MapInput(input);
     }
 
     private static CliCommand MapInput(string input)
         => input switch
         {
-            "Add Transaction" => CliCommand.AddTransaction,
-            "Mine Block" => CliCommand.MineBlock,
-            "Print Chain" => CliCommand.PrintBlockChain,
-            "Quit" => CliCommand.Quit,
+            CliCommandStrings.AddTransaction => CliCommand.AddTransaction,
+            CliCommandStrings.MineBlock => CliCommand.MineBlock,
+            CliCommandStrings.PrintChain => CliCommand.PrintBlockChain,
+            CliCommandStrings.Quit => CliCommand.Quit,
             _ => throw new ArgumentOutOfRangeException(nameof(input), input, null),
         };
 }
